25685beab10ffc0fa55d156631ace84e09174d9b
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-13 17:16:24 +0100

    update dockerfiles and app tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@apollo/client";
+import { HELLO_QUERY } from "./queries";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery(HELLO_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>GraphQL React App</h1>
+      <p>Response from GraphQL API: {data.hello}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;

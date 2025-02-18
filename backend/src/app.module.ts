608525a3f7fb22084da205dcc25f64f3e632bc7b
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-18 12:53:01 +0100

    Add fix for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,25 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { ExampleResolver } from './example/example.resolver';
 import { WelcomeController } from './welcome/welcome.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongo:27017/mydb'), // Connect to MongoDB
+    MongooseModule.forRoot('mongodb://mongo:27017/mydb'),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true, // Generates schema automatically
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      plugins: [],
+      csrfPrevention: false,
     }),
   ],
-  providers: [ExampleResolver],
+
+  providers: [AppService, ExampleResolver],
   controllers: [WelcomeController],
 })
 export class AppModule { }
